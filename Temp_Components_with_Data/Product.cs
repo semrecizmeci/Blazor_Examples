4c34f890f428c6abe51587ea454480b2e6a94f39
Sadık Emre Çizmecioğlu <121570540+semrecizmeci@users.noreply.github.com>
2023-09-04 17:15:50 +0300

    Create Product.cs

---
+++ Temp_Components_with_Data/Product.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp1_education.Models
+{
+    public class Products
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public long price { get; set; }
+    }
+}
