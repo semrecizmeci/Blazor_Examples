e0ef129f9212b8edf49dc96a638100208edabfcb
Sadık Emre Çizmecioğlu <121570540+semrecizmeci@users.noreply.github.com>
2023-09-06 14:12:26 +0300

    Create DateTimeBaseComponent.cs

---
+++ @inherits/DateTimeBaseComponent.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.AspNetCore.Components;
+using System;
+
+
+namespace _8hoursBlazorLessonExamples.BaseComponent
+{
+    public class DateTimeBaseComponent:ComponentBase
+    {
+        public DateTimeBaseComponent()
+        {
+
+        }
+        public DateTime datetime { get; set; }
+    }
+}
